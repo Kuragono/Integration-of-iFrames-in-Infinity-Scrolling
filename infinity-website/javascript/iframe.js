1f66f9e3cb77bcfb74c8f21f052176153279aacb
Kuragono <tiago.carreira@outlook.com>
2022-06-27 14:24:56 +0200

    Add resizeObserver

--- infinity-website/javascript/iframe.js
+++ infinity-website/javascript/iframe.js
@@ -5,12 +5,12 @@ const size = document.getElementById('size');
 /*-------------------------- functions --------------------------*/
 
 const getHeight = () => (Math.floor(Math.random() * 800));
-const getWidth = () => (Math.floor(Math.random() * 1080));
+//const getWidth = () => (Math.floor(Math.random() * 1080));
 
-/*-------------------------- styles --------------------------*/
+/*-------------------------- styles --------------------------cd*/
 
 size.style.height = `${getHeight()}px`;
-size.style.width = `${getWidth()}px`;
+size.style.width = '100%';
 size.style.display = 'block';
 
 size.style.overflow = 'hidden';
@@ -18,8 +18,14 @@ size.scrolling = 'no';
 
 /*-------------------------- main code --------------------------*/
 
-let height = size.style.height;
-let width = size.style.width;
-let sizes = {width, height};
+const divElem = document.querySelector('body > div');
 
-window.parent.postMessage(sizes);
+const resizeObserver = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    console.log('Size changed',entry.target.scrollHeight);
+    let data = [size.style.width, entry.target.scrollHeight];
+    window.parent.postMessage(data);
+  }
+});
+
+resizeObserver.observe(divElem);
